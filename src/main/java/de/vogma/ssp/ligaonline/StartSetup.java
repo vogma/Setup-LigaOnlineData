f546984ed988f671102ccf00f6f6423d82e66de8
Marco <Marco@localhost>
2017-06-25 13:20:32 +0200

    Added Apache ScriptRunner Dependency

--- src/main/java/de/vogma/ssp/ligaonline/StartSetup.java
+++ src/main/java/de/vogma/ssp/ligaonline/StartSetup.java
@@ -7,7 +7,6 @@ import de.vogma.ssp.ligaonline.entitys.Stadion;
 import de.vogma.ssp.ligaonline.entitys.Team;
 import de.vogma.ssp.ligaonline.manager.ConnectionManager;
 import de.vogma.ssp.ligaonline.manager.PersistenceManager;
-import de.vogma.ssp.ligaonline.manager.DataBaseInitiator;
 import de.vogma.ssp.ligaonline.xmlparser.GoalParser;
 import de.vogma.ssp.ligaonline.xmlparser.MatchParser;
 import de.vogma.ssp.ligaonline.xmlparser.PlayerParser;
@@ -25,30 +24,29 @@ import java.sql.SQLException;
 import java.util.List;
 
 import javax.xml.parsers.ParserConfigurationException;
+import org.apache.ibatis.jdbc.ScriptRunner;
 import org.xml.sax.SAXException;
 
-
-
 public class StartSetup {
-
+    
     private static InputStream matchStream;
     private static InputStream teamStream;
     private static InputStream playerStream;
     private static InputStream stadionStream;
     private static InputStream goalStream;
-
+    
     private static List<Match> matches;
     private static List<Team> teams;
     private static List<Player> player;
     private static List<Stadion> stadion;
     private static List<Goal> goals;
-
+    
     private static MatchParser matchParser;
     private static PlayerParser playerParser;
     private static TeamParser teamParser;
     private static StadionParser stadionParser;
     private static GoalParser goalParser;
-
+    
     public static void main(String[] args) {
         try {
             System.out.println("Initialisiere Streams...");
@@ -62,10 +60,10 @@ public class StartSetup {
             System.out.println("Persistiere Daten...");
             persistData();
         } catch (Exception ex) {
-            System.err.println("ERROR: "+ex.getLocalizedMessage());
+            System.err.println("ERROR: " + ex.getLocalizedMessage());
         }
     }
-
+    
     private static void initializeStreams() {
         matchStream = StartSetup.class.getResourceAsStream("/matches.xml");
         teamStream = StartSetup.class.getResourceAsStream("/teams.xml");
@@ -73,7 +71,7 @@ public class StartSetup {
         stadionStream = StartSetup.class.getResourceAsStream("/stadions.xml");
         goalStream = StartSetup.class.getResourceAsStream("/matches.xml");
     }
-
+    
     private static void initializeParser() {
         matchParser = new MatchParser();
         playerParser = new PlayerParser();
@@ -81,7 +79,7 @@ public class StartSetup {
         stadionParser = new StadionParser();
         goalParser = new GoalParser();
     }
-
+    
     private static void persistData() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
         PersistenceManager.persistStadions(stadion);
         PersistenceManager.persistTeams(teams);
@@ -89,19 +87,19 @@ public class StartSetup {
         PersistenceManager.persistMatches(matches);
         PersistenceManager.persistGoals(goals);
     }
-
+    
     private static void parseXML() throws SAXException, IOException, ParserConfigurationException {
-
+        
         matches = matchParser.parseXML(matchStream);
         teams = teamParser.parseXML(teamStream);
         player = playerParser.parseXML(playerStream);
         stadion = stadionParser.parseXML(stadionStream);
         goals = goalParser.parseXML(goalStream);
-
+        
         teams = linkTeamsWithStadion(teams, stadion);
         matches = linkMatchesWithTables(matches, teams, stadion);
     }
-
+    
     private static void testGoals() {
         int c = 0;
         for (Goal goal : goals) {
@@ -114,9 +112,9 @@ public class StartSetup {
         }
         System.out.println(c);
     }
-
+    
     private static List<Match> linkMatchesWithTables(List<Match> matches, List<Team> teams, List<Stadion> stadionList) {
-
+        
         for (Match match : matches) {
             for (Stadion stadion : stadionList) {
                 if (match.getTeamName1().equals(stadion.getTeamName())) {
@@ -135,10 +133,10 @@ public class StartSetup {
                 }
             }
         }
-
+        
         return matches;
     }
-
+    
     private static List<Team> linkTeamsWithStadion(List<Team> teams, List<Stadion> stadion) {
         for (Team team : teams) {
             for (Stadion stadionItem : stadion) {
@@ -149,13 +147,14 @@ public class StartSetup {
         }
         return teams;
     }
-
+    
     private static void runDatabaseSetupScript() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException, FileNotFoundException, IOException {
         try (Connection connection = ConnectionManager.getConnection()) {
-            DataBaseInitiator scriptRunner = new DataBaseInitiator(connection,false);
+            ScriptRunner scriptRunner = new ScriptRunner(connection);
+            scriptRunner.setLogWriter(null); // Erzeuge keine Logmeldungen -> nur Fehler
             Reader reader = new BufferedReader(new InputStreamReader(StartSetup.class.getResourceAsStream("/database/init.sql")));
-            scriptRunner.startInit(reader);
+            scriptRunner.runScript(reader);
         }
     }
-
+    
 }
